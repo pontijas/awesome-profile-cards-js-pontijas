cd3eac25c2c969bd518e11cecbd353b418e4dc05
root <root@DESKTOP-K4RHFBE.localdomain>
2021-01-29 16:26:06 +0100

    set styles and js for color parlette number 4

--- _src/assets/js/colorpalette.js
+++ _src/assets/js/colorpalette.js
@@ -5,16 +5,18 @@ console.log('soy color palette');
 const inputPalette1 = document.querySelector(".js-palette1");
 const inputPalette2 = document.querySelector(".js-palette2");
 const inputPalette3 = document.querySelector(".js-palette3");
+const inputPalette4 = document.querySelector(".js-palette4");
 const cardElement = document.querySelector(".js-card");
 
 function paintPalette() {
   cardElement.classList.remove("js-card-palette1");
   cardElement.classList.remove("js-card-palette2");
   cardElement.classList.remove("js-card-palette3");
+  cardElement.classList.remove("js-card-palette4");
   let selectedPalette = document.querySelector(".js-radio:checked");
-  console.log(selectedPalette);
+  console.log('selected palette', selectedPalette);
   selectedPalette = selectedPalette.value;
-  console.log(selectedPalette);
+  console.log('selected palette value', selectedPalette);
   cardElement.classList.add("js-card-palette" + selectedPalette);
   localStorage.setItem("palette", selectedPalette);
 
@@ -23,3 +25,4 @@ function paintPalette() {
 inputPalette1.addEventListener("click", paintPalette);
 inputPalette2.addEventListener("click", paintPalette);
 inputPalette3.addEventListener("click", paintPalette);
+inputPalette4.addEventListener("click", paintPalette);
